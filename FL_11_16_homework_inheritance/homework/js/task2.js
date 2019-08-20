fd198b9bdea61bdd27a4e2dcf0fedd688cb318f9
insaneAlex <syvoplias76@gmail.com>
2019-08-20 23:32:48 +0300

    eslint error removed

--- FL_11_16_homework_inheritance/homework/js/task2.js
+++ FL_11_16_homework_inheritance/homework/js/task2.js
@@ -3,15 +3,22 @@
 // }
 
 const myCreate = function (obj, props) {
-    function F() { }
+    function F() {
+        return
+    }
     F.prototype = obj;
-
-    if (typeof (props) === "object") {
-        for (prop in props) {
-            if (props.hasOwnProperty((prop))) {
+    if (typeof props === 'object') {
+        for (let prop in props) {
+            if (Object.prototype.hasOwnProperty.call(props, prop)) {
                 F[prop] = props[prop];
             }
         }
     }
     return new F();
 };
+
+const obj1 = { prop: 5 };
+const obj2 = myCreate(obj1);
+
+Object.getPrototypeOf(obj2) === obj1; // => true
+obj2.prop; // => 5
