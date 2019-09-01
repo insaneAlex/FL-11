38352c2b562a607a563ee6ebc16eda52ba5bd658
insaneAlex <syvoplias76@gmail.com>
2019-09-01 21:40:38 +0300

    s

--- FL11_HW20_ReactJS_JSX/reactive-app/src/App.js
+++ FL11_HW20_ReactJS_JSX/reactive-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/header/header';
-import Aux from './hoc/Auxx/Aux';
+import Aux from './hoc/Auxx/Auxx';
 import './App.css';
 
 class App extends Component {
