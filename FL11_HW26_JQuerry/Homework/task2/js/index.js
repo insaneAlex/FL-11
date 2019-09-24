ecc101000d000fad5580ad3c15fa8b9d20a456ef
insaneAlex <syvoplias76@gmail.com>
2019-09-24 23:56:30 +0300

    update

--- FL11_HW26_JQuerry/Homework/task2/js/index.js
+++ FL11_HW26_JQuerry/Homework/task2/js/index.js
@@ -1,6 +1,8 @@
 const $list = $(".list");
 const $input = $("#add-input");
 const $add = $("#add-submit");
+const $delete = $("button.item-remove");
+const $item = $(".item-text");
 
 const todos = [
   {
@@ -12,3 +14,31 @@ const todos = [
     done: true
   }
 ];
+const delClass = () => {
+  $(".item button").addClass("item-remove");
+  $(".item-remove").bind("click", function () {
+    $(this).parent().remove()
+  });
+}
+const completeTask = () => {
+  $(".item span").bind("click", function () {
+    $(this).css({ "font-style": "italic", "text-decoration": "line-through" });
+  });
+}
+$add.on('click', function (e) {
+  const temp = $input.val();
+  if (temp === '') {
+    return
+  }
+  const newChild = `<li class="item"><span class="item-text">${temp}</span><button>Remove</button></li>`;
+  $list.append(newChild);
+  delClass();
+  completeTask();
+  e.preventDefault();
+});
+$delete.click(function () {
+  $(this).parent().remove();
+})
+$item.on('click', function () {
+  $(this).css({ "font-style": "italic", "text-decoration": "line-through" })
+})
